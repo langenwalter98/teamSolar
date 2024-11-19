1f4c080034adeb6abca8fb40a7e046eb1e9b9f6c
Alexander Langenwalter <alex.langenwalter@icloud.com>
2024-11-19 00:57:55 -0700

    added interactions with javascript & finished dashboard grid

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
 // test to ensure the file is working as intended
 console.log("your index.js file is loaded correctly!");
-
-
+// global variables that will be used to set a custom color in the .hover functions
+hover_color = "#85a0c2"
+// underline interaction
+$(".sidebar nav p").hover(function(){
+    $(this).css("text-decoration-line", "underline");
+    }, function(){
+    $(this).css("text-decoration-line", "none");
+  });
+// color change interaction
+$(".headerIcons .fa-magnifying-glass").hover(function(){
+    $(this).css("color", hover_color);
+    }, function(){
+    $(this).css("color", "white");
+  });
+$(".headerIcons .fa-bell").hover(function(){
+    $(this).css("color", hover_color);
+    }, function(){
+    $(this).css("color", "white");
+  });
+$(".headerIcons .fa-circle-info").hover(function(){
+    $(this).css("color", hover_color);
+    }, function(){
+    $(this).css("color", "white");
+  });
 
 // COMMENT COPY
